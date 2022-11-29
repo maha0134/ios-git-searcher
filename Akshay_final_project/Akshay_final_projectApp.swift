fe04a465a2e0b077b5a53396c68cc6f381869223
Akshay Mahajan <maha0134@algonquinlive.com>
2022-11-29 17:37:44 -0500

    Initial Commit

---
+++ Akshay_final_project/Akshay_final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Akshay_final_projectApp.swift
+//  Akshay_final_project
+//
+//  Created by AKSHAY MAHAJAN on 2022-11-29.
+//
+
+import SwiftUI
+
+@main
+struct Akshay_final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
